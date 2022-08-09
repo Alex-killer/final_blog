6fcfc3296cfd24b4d8ba98210df1092539800a95
alex-killer <onefrank@bk.ru>
2022-08-09 15:57:41 +0700

    1

--- resources/views/blog/includes/footer.blade.php
+++ resources/views/blog/includes/footer.blade.php
@@ -4,8 +4,7 @@
         <nav class="nav footer-bottom-nav">
             <a href="#!">Privacy & Policy</a>
             <a href="#!">Terms</a>
-            <a href="#!">Site Map</a>
         </nav>
-        <p class="mb-0">© Edica. 2020 <a href="https://www.bootstrapdash.com" target="_blank" rel="noopener noreferrer" class="text-reset">bootstrapdash</a> . All rights reserved.</p>
+        <p class="mb-0">© 2022 . All rights reserved.</p>
     </div>
 </div>
